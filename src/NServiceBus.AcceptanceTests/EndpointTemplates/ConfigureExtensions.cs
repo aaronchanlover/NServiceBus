56993b976c34c343c4200ee64f74d09c75f44d7c
Tim Bussmann <tim.bussmann@particular.net>
2017-03-17 16:00:26 +0100

    move IConfigureEndpointTestExecution to acceptance tests project

--- src/NServiceBus.AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
+++ src/NServiceBus.AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
@@ -7,43 +7,33 @@
 
     public static class ConfigureExtensions
     {
-        public static Task DefineTransport(this EndpointConfiguration config, RunSettings settings, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
+        public static async Task DefineTransport(this EndpointConfiguration config, RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
         {
-            if (TestSuiteConstraints.Current.TransportConfiguration == null)
+            var transportConfiguration = TestSuiteConstraints.Current.TransportConfiguration;
+            if (transportConfiguration == null)
             {
                 throw new Exception($"No valid transport configuration found. Configure a transport by assigning {nameof(TestSuiteConstraints)}.{nameof(TestSuiteConstraints.TransportConfiguration)}.");
             }
 
-            return ConfigureTestExecution(TestSuiteConstraints.Current.TransportConfiguration, config, settings, endpointCustomizationConfiguration.EndpointName, endpointCustomizationConfiguration.PublisherMetadata);
+            await transportConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, config, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+            runDescriptor.OnTestCompleted(_ => transportConfiguration.Cleanup());
         }
 
-        public static Task DefinePersistence(this EndpointConfiguration config, RunSettings settings, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
+        public static async Task DefinePersistence(this EndpointConfiguration config, RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
         {
-            if (TestSuiteConstraints.Current.PersistenceConfiguration == null)
+            var persistenceConfiguration = TestSuiteConstraints.Current.PersistenceConfiguration;
+            if (persistenceConfiguration == null)
             {
                 throw new Exception($"No valid persistence configuration found. Configure a persistence by assigning {nameof(TestSuiteConstraints)}.{nameof(TestSuiteConstraints.PersistenceConfiguration)}.");
             }
 
-            return ConfigureTestExecution(TestSuiteConstraints.Current.PersistenceConfiguration, config, settings, endpointCustomizationConfiguration.EndpointName, endpointCustomizationConfiguration.PublisherMetadata);
+            await persistenceConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, config, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+            runDescriptor.OnTestCompleted(_ => persistenceConfiguration.Cleanup());
         }
 
         public static void RegisterComponentsAndInheritanceHierarchy(this EndpointConfiguration builder, RunDescriptor runDescriptor)
         {
             builder.RegisterComponents(r => { RegisterInheritanceHierarchyOfContextOnContainer(runDescriptor, r); });
-        }
-
-        static async Task ConfigureTestExecution(IConfigureEndpointTestExecution configurer, EndpointConfiguration config, RunSettings settings, string endpointName, PublisherMetadata publisherMetadata)
-        {
-            await configurer.Configure(endpointName, config, settings, publisherMetadata).ConfigureAwait(false);
-
-            ActiveTestExecutionConfigurer cleaners;
-            var cleanerKey = "ConfigureTestExecution." + endpointName;
-            if (!settings.TryGet(cleanerKey, out cleaners))
-            {
-                cleaners = new ActiveTestExecutionConfigurer();
-                settings.Set(cleanerKey, cleaners);
-            }
-            cleaners.Add(configurer);
         }
 
         static void RegisterInheritanceHierarchyOfContextOnContainer(RunDescriptor runDescriptor, IConfigureComponents r)
@@ -54,12 +44,6 @@
                 r.RegisterSingleton(type, runDescriptor.ScenarioContext);
                 type = type.BaseType;
             }
-        }
-
-        enum TestDependencyType
-        {
-            Transport,
-            Persistence
         }
     }
 }
